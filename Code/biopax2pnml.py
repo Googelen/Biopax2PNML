af7da1585eb81f41982016b2c0d412773b0911be
Googelen <laserluc@live.nl>
2014-05-02 21:48:10 +0200

    Added the validator
    Added support for the validator, giving the net as input

--- Code/biopax2pnml.py
+++ Code/biopax2pnml.py
@@ -1,10 +1,13 @@
 import biopax
 import pnml
 from models import *
+import validator
 
 class Convertor:
 	def __init__(self, inputfile, outputfile):
 		reader = biopax.Reader()
 		net = reader.read(inputfile)
+		validatorThingy = validator.Validator(net)
+		validatorThingy.checkArcs()
 		writer = pnml.Writer(net)
 		writer.write(outputfile)
