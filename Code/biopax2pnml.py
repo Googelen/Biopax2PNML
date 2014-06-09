149f353ee053aab1c5b314e4c55b3244c3eb90ae
Googelen <laserluc@live.nl>
2014-06-09 15:58:59 +0200

    Added classifier
    added the classifier

--- Code/biopax2pnml.py
+++ Code/biopax2pnml.py
@@ -2,6 +2,7 @@ import biopax
 import pnml
 from models import *
 import validator
+import classifier
 
 class Convertor:
 	def __init__(self, inputfile, outputfile):
@@ -9,5 +10,7 @@ class Convertor:
 		net = reader.read(inputfile)
 		validatorThingy = validator.Validator(net)
 		validatorThingy.checkArcs()
+		classify = classifier.Classifier(net)
+		classify.classify()
 		writer = pnml.Writer(net)
 		writer.write(outputfile)
