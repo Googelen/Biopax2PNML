6274d200eba55076ed7cc949acaf3f3ef86d3413
Googelen <laserluc@live.nl>
2014-06-09 18:49:38 +0200

    Added making a fake petrinet
    Still not yet found a good petrinet, and this gives errors

--- Code/test/classifier/NetClassifierTest.py
+++ Code/test/classifier/NetClassifierTest.py
@@ -19,43 +19,71 @@ class EmptyNetTestCase(unittest.TestCase):
 class extended_simple_and_extended_free_choice_testcase(unittest.TestCase):
 	def setUp(self):
 		net = PetriNet("This petri net is empty")
+		net1 = PetriNet("This petri net is empty")
+		net2= PetriNet("This petri net is empty")
+		places=[]
+		for i in range(0,4):
+			places.append(Place("i/"+str(i),"place"+str(i)))
+		transitions = []
+		for i in range(0,3):
+			transitions.append(Transition())
+		
+		arcs = []
+		arcs.append(Arc(places[0],transitions[0]))
+		arcs.append(Arc(transitions[0],places[1]))
+		arcs.append(Arc(places[1],transitions[1]))
+		arcs.append(Arc(transitions[1],places[2]))
+		arcs.append(Arc(places[2],transitions[2]))
+		arcs.append(Arc(transitions[2],places[3]))
+
+		arcs1=[]
+		arcs1.append(Arc(places[0],transitions[0]))
+		arcs1.append(Arc(transitions[0],places[1]))
+		arcs1.append(Arc(places[1],transitions[1]))
+		arcs1.append(Arc(transitions[0],places[2]))
+		arcs1.append(Arc(places[2],transitions[2]))
+		arcs1.append(Arc(transitions[1],places[3]))
+		arcs1.append(Arc(transitions[1],places[3]))
+
+		arcs2=[]
+		arcs2.append(Arc(places[0],transitions[0]))
+		arcs2.append(Arc(transitions[0],places[1]))
+		arcs2.append(Arc(places[1],transitions[1]))
+		arcs2.append(Arc(transitions[0],places[2]))
+		arcs2.append(Arc(transitions[1],places[3]))
+
+		for place in places:
+			net.newPlace(place)
+			net1.newPlace(place)
+			net2.newPlace(place)
+		for transition in transitions:
+			net.newTransition(transition)
+			net1.newTransition(transition)
+			net2.newTransition(transition)
+		for arc in arcs:
+			net.newArc(arc)
+		for arc in arcs1:
+			net1.newArc(arc)
+		for arc in arcs2:
+			net2.newArc(arc)
+
 		c = NetClassifier(net)
+		self.c = c.classify()
+		d = NetClassifier(net1)
+		self.d = d.classify()
+		e = NetClassifier(net2)
+		self.e = e.classify()
 
-		# This should all be defined in net (above) instead of as dicts.
-
-		# test_places1 = {
-		# 'test1': (None,set(['0','1','2'])),
-		# 'test2': (None,set(['3','4','5'])),
-		# 'test3':(None,set(['1','2','0']))
-		#
-		# }
-		#
-		# test_places2 = {
-		# 'test1': (None,set(['0','1','2'])),
-		# 'test2': (None,set(['3','4','5'])),
-		# 'test3':(None,set(['1','2']))
-		#
-		# }
-		# test_places3 = {
-		# 'test1': (None,set(['0','1','2'])),
-		# 'test2': (None,set(['3','4','5'])),
-		# 'test3':(None,set(['1','2','3']))
-		#
-		# }
-
+		
 	def test_extended_simple(self):
-		# These should not call c.is_extended_...
-		# Rather call c.classify and compare the classes in the resulting dict.
-		self.assertEqual(True,c.is_extended_simple_choice(test_places1))
-		self.assertEqual(True,c.is_extended_simple_choice(test_places2))
-		self.assertEqual(False,c.is_extended_simple_choice(test_places3))
+		self.assertEqual(True,self.c['exteded_simple'])
+		self.assertEqual(False,self.d['exteded_simple'])
+		self.assertEqual(False,self.e['exteded_simple'])
 		
 	def test_extended_free_choice(self):
-		# These should not call c.is_extended_...
-		# Rather call c.classify and compare the classes in the resulting dict.
-		self.assertEqual(True,c.is_extended_free_choice(test_places1))
-		self.assertEqual(False, c.is_extended_free_choice(test_places2))
-		self.assertEqual(False,c.is_extended_free_choice(test_places3))
+		self.assertEqual(False,self.c['exteded_free_choice'])
+		self.assertEqual(False,self.d['exteded_free_choice'])
+		self.assertEqual(False,self.e['exteded_free_choice'])
 		
 
 if __name__ == '__main__':
