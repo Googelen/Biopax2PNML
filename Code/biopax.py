4b16ea5d3d819d1e16a5ae6c993986468ab7a903
Googelen <laserluc@live.nl>
2014-07-01 22:09:27 +0200

    Changed the catalysis query and function
    I now understand better how it works, but can't really program it correctly
    It will not return the right transition
    Yes, there are a lot of loops again, but that is because the net is not yet fully generated
    Also I have noticed that the returned values of the query with the biochemical reaction always has a length of 4, so it skipps the reactions with more than 1 left or right place or just ignores some of them.

--- Code/biopax.py
+++ Code/biopax.py
@@ -26,12 +26,11 @@ class Reader:
 		"""
 
 		for x in graph.query(query):
-			counter=0
-			while(x.left):
-				self.processQuery(x.left[counter],x.displayNameLeft[counter], x.right[0], x.displayNameRight[0])
-				counter++
+			self.processQuery(x.left, x.displayNameLeft, x.right, x.displayNameRight)
 			#print("left: %s right: %s" % x)
-		readCatalysis(inputfile)
+
+		self.readCatalysis(inputfile)
+
 		return self.net
 
 		#print("graph has %s statements." % len(graph))
@@ -42,24 +41,58 @@ class Reader:
 	def readCatalysis(self, inputfile):
 		graph = rdflib.Graph()
 		result = graph.parse(inputfile)
-
-
 		query = """
 			PREFIX bp: <http://www.biopax.org/release/biopax-level3.owl#>
-			SELECT  ?controller ?controlled ?displayNameController ?displayNameControlled
+			SELECT  ?controller ?displayNameController ?controlled  ?leftPlaceControlled ?rightPlaceControlled
 			WHERE {
 			    ?Catalysis bp:controller ?controller . 
 			    ?Catalysis bp:controlled ?controlled .
-			    ?controller bp:displayName ?displayNameController . 
-			    ?controlled bp:displayName ?displayNameControlled
+			    ?controller bp:displayName ?displayNameController .
+			    ?controlled bp:left ?leftPlaceControlled.
+			    ?controlled bp:left ?rightPlaceControlled
+
 			}
 		"""
+		tempPlaces = []
+		tempArcs = []
 		for x in graph.query(query):
-			#This works both ways
-			processQuery(x.controller, x.displayNameController,x.controlled,x.displayNameControlled)
-			processQuery(x.controlled, x.displayNameControlled,x.controller,x.displayNameController)
-		
-	
+			# the controlled is not a place, but a entire reaction, so this is wrong, it should go directly into the transition of the reaction which is in the 'controlled'
+			#it should find the transition which is the pretransition of the place right of the reaction which it points to
+			transition = self.findTransition(x.leftPlaceControlled, x.rightPlaceControlled)
+			catalysisPlace = Place(x.controller,x.displayNameController)
+
+			tempArcs.append(Arc(catalysisPlace,transition))
+			tempArcs.append(Arc(transition,catalysisPlace))
+			tempPlaces.append(catalysisPlace)
+		i=0
+		while i<len(tempPlaces):
+			self.net.newPlace(tempPlaces[i])
+			i += 1
+		j=0
+		while j<len(tempArcs):
+			self.net.newArc(tempArcs[j])
+			j+=1
+
+
+	def findTransition(self,leftPlace,rightPlace):
+		"""
+		find the transition inbeteen the places defined by the not yet id-ified (making up words here) string which represends the place left and right
+
+		"""
+		leftPlaceId = leftPlace.rsplit('/',1)[1]
+		rightPlaceId = rightPlace.rsplit('/',1)[1]
+		transition=[]
+		for arc in self.net.arcs:
+			if(isinstance(arc.source, Place) and arc.source.id==leftPlaceId):
+				transition = arc.target
+				print(arc.source.id +" "+leftPlaceId)
+
+		for arc in self.net.arcs:
+			if(isinstance(arc.target, Place) and arc.target.id==rightPlaceId):
+				if(transition is arc.source):
+					return transition
+
+
 	def processQuery(self, left,nameLeft, right,nameRight):
 		placeLeft = Place(left, nameLeft)
 		placeRight = Place(right, nameRight)
@@ -72,7 +105,7 @@ class Reader:
 		self.net.newTransition(transition)
 		self.net.newArc(arcleft)
 		self.net.newArc(arcRight)
-		
+
 	def readPlaces(self):
 		return self.net.places
 	def readTransitions(self):
