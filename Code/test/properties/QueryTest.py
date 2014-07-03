6c0a6039253baccff3aedbf457f834e0a6d587b6
Googelen <laserluc@live.nl>
2014-07-03 14:35:43 +0200

    Fixed a bug
    Had twice the same name

--- Code/test/properties/QueryTest.py
+++ Code/test/properties/QueryTest.py
@@ -8,28 +8,36 @@ class Query_Test:
 
 	def test_query(self,inputfile):
 		query="""
-			PREFIX bp: <http://www.biopax.org/release/biopax-level3.owl#>
-			SELECT  ?left ?displayNameLeft ?right ?displayNameRight ?type ?classs
-			WHERE {
-				?classs bp:left ?left . 
-                ?classs bp:right ?right .
-                ?left bp:displayName ?displayNameLeft . 
-                ?right bp:displayName ?displayNameRight
-                
-			}
+			 PREFIX bp: <http://www.biopax.org/release/biopax-level3.owl#>
+                        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                        SELECT  ?about ?child ?displayName
+                        WHERE {
+                            {
+                                SELECT DISTINCT ?instance ?about {
+                                    ?class rdfs:subClassOf bp:Conversion.
+                                    ?instance a ?class;
+                                              rdf:about ?about
+                                }
+                            }
+                            
+                            OPTIONAL {
+                                ?instance bp:left/bp:right ?child.
+                                ?child bp:displayName ?displayName
+                            }
+                        }
 		"""
 		
-		result = self.test_query(inputfile,query)
+		result = self.execute_query(inputfile,query)
 
 		print(len(result))
 		for x in result:
-			print(x.left)
-			print(x.displayNameLeft)
-			print(x.right)
-			print(x.displayNameRight)
+			print(x.about)
+			print(x.child)
+			print(x.displayName)
 			print("")
 			
-	def test_query(self, inputfile,query):
+	def execute_query(self, inputfile,query):
 		graph = rdflib.Graph()
 		graph.parse(inputfile)
 		return graph.query(query)
