4c3b9ff6a8567fa15d84a2fab2eef3fe2490510d
Franz Geiger <mail@fx-g.de>
2014-07-03 16:58:56 +0200

    Create SPARQL query which matches all subclasses of bp:Conversion and retrieve all children

--- Code/test/properties/QueryTest.py
+++ Code/test/properties/QueryTest.py
@@ -1,60 +1,41 @@
 import rdflib
-from rdflib import plugin
 
-class Query_Test:
+
+class QueryTest:
 	def __init__(self):
-		self.test_query("WP78_70014.owl")
+
+		self.graph = rdflib.Graph()
+		self.graph.parse("../../WP78_70014.owl")
+		self.graph.parse("../../biopax-level3.owl")
+		self.test_query()
 		return
 
-	def test_query(self,inputfile):
-		query="""
-			 PREFIX bp: <http://www.biopax.org/release/biopax-level3.owl#>
-                        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-                        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-                        SELECT  ?about ?child ?displayName
-                        WHERE {
-                            {
-                                SELECT DISTINCT ?instance ?about 
-                                {
-                                    ?class rdfs:subClassOf* bp:Conversion.
-                                    ?instance a ?class;
-                                              rdf:about ?about
-                                }
-                            }
-                            
-                            OPTIONAL {
-                                ?instance bp:left/bp:right ?child.
-                                ?child bp:displayName ?displayName
-                            }
-                        }
-		"""
-		
-
-		query="""
+	def test_query(self):
+		query = """
 			PREFIX bp: <http://www.biopax.org/release/biopax-level3.owl#>
-            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-			SELECT  ?subclass
+			SELECT  ?class ?conversion ?child ?childName ?direction
 			WHERE {
-				 bp:BiochemicalReaction rdfs:subClassOf ?subclass
+				?class rdfs:subClassOf+ bp:Conversion.
+				?conversion a ?class.
+				{
+					?conversion bp:left ?child.
+					?child bp:displayName ?childName
+				} UNION {
+					?conversion bp:right ?child.
+					?child bp:displayName ?childName
+				}
+				OPTIONAL { ?conversion bp:conversionDirection ?direction }
 			}
-
-
-
 		"""
 
-		result = self.execute_query(inputfile,query)
+		result = self.graph.query(query)
 
 		print(len(result))
+
 		for x in result:
-			print(x.subclass)
-			#print(x.child)
-			#print(x.displayName)
+			for prop in x:
+				print(prop)
 			print("")
-			
-	def execute_query(self, inputfile,query):
-		graph = rdflib.Graph()
-		graph.parse(inputfile)
-		return graph.query(query)
+
 		
-test = Query_Test()
+test = QueryTest()
