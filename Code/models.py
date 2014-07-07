2dcb67151a08956af86630e4cf9f8c40a5e83e96
Franz Geiger <mail@fx-g.de>
2014-07-07 20:15:25 +0200

    Add ID and comparison to both Place and Transition. PetriNet has sets for members.

--- Code/models.py
+++ Code/models.py
@@ -1,32 +1,46 @@
 class PetriNet:
 	def __init__(self, description):
 		self.description = description;
-		self.places = []
-		self.transitions = []
-		self.arcs = []
+		self.places = set()
+		self.transitions = set()
+		self.arcs = set()
 
 	def newPlace(self, place):
-		for exisitingplace in self.places:
-			if exisitingplace.id == place.id:
-				print('Found duplicate place')
-				return
+		self.places.add(place)
 
-		self.places.append(place)
 	def newTransition(self, transition):
-		self.transitions.append(transition)
-	def newArc(self, arc):
-		self.arcs.append(arc)
+		self.transitions.add(transition)
 
-class Place:
+	def newArc(self, arc):
+		self.arcs.add(arc)
+
+
+class Node:
+	def __init__(self, uid):
+		self.id = uid
+
+	def __eq__(self, other):
+		return isinstance(other, self.__class__) and (self.id == other.id)
+
+	def __ne__(self, other):
+		return not self.__eq__(other)
+
+	def __hash__(self):
+		return hash(self.id)
+
+
+class Place(Node):
 	def __init__(self, uid, description):
-		self.id = uid.rsplit('/',1)[1]
+		Node.__init__(self, uid.rsplit('/', 1)[1])  # split should be executed before calling constructor.
 		self.description = description
+
 
 class Arc:
 	def __init__(self, source, target):
 		self.source = source
 		self.target = target
 
-class Transition:
-	def __init__(self):
-		return
+
+class Transition(Node):
+	def __init__(self, uid):
+		Node.__init__(self, uid.rsplit('/', 1)[1])  # split should be executed before calling constructor.
