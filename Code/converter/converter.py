bb5fa81ade686d8c5a767275ab9118e69d2abb05
Googelen <laserluc@live.nl>
2014-07-20 21:55:50 +0200

    Started with irreversible class
    Started implementing irreversible controller class. It is not yet
    finished.

--- Code/converter/converter.py
+++ Code/converter/converter.py
@@ -34,6 +34,20 @@ class BiopaxConverter(object):
 		"""
 		raise NotImplementedError
 
+	def get_direction(self, control):
+		direction = Direction.unknown
+
+		if control.direction is REVERSIBLE:
+			direction = Direction.reversible
+
+		if control.direction is PHYSIOL_LTR or control.direction is IRR_LTR:
+			direction = Direction.left_to_right
+
+		if control.direction is PHYSIOL_RTL or control.direction is IRR_RTL:
+			direction = Direction.right_to_left
+
+		return direction
+
 
 class ConversionConverter(BiopaxConverter):
 
@@ -191,16 +205,149 @@ class ActivatingControlConverter(BiopaxConverter):
 
 		return transitions
 
-	def get_direction(self, control):
-		direction = Direction.unknown
+class ActivationInhibitionAlossteric(BiopaxConverter):
+	def __init__(self, graph, petri_net):
+		"""Converts members of class Control with controlType Inhibition, Inhibition-Alossteric and Activation-Alossteric from BioPAX to a Petri Net.
 
-		if control.direction is REVERSIBLE:
-			direction = Direction.reversible
+		:param graph: rdflib.Graph
+		:param petri_net: PetriNet
+		"""
+		BiopaxConverter.__init__(self, graph, petri_net)
 
-		if control.direction is PHYSIOL_LTR or control.direction is IRR_LTR:
-			direction = Direction.left_to_right
+	query = """
+		PREFIX bp: <http://www.biopax.org/release/biopax-level3.owl#>
+		SELECT *
+		WHERE {
+			?controlClass rdfs:subClassOf+ bp:Control.
 
-		if control.direction is PHYSIOL_RTL or control.direction is IRR_RTL:
-			direction = Direction.right_to_left
+			{ ?relation a bp:controller } UNION { ?relation a bp:cofactor }
 
-		return direction
+			?interaction
+				a ?controlClass;
+				bp:controlled ?controlled;
+				?relation ?participant.
+
+			OPTIONAL { ?participant bp:displayName ?participantName }
+			OPTIONAL { ?interaction bp:catalysisDirection ?direction }
+			{ ?interaction a bp:Catalysis }	UNION { ?interaction bp:controlType ?types } 
+
+			VALUES (?types) { 
+			("INHIBITION")
+			("INHIBITION-ALOSSTERIC")
+			("ACTIVATION-ALOSSTERIC")
+			("ACTIVATION")
+			}
+		}
+	"""
+
+	def convert(self):
+		for control in self.graph.query(self.query):
+			self.add_control(control)
+
+	def add_control(self, control):
+		"""Adds a controller to a controlled transition.
+
+		Catalysis can only be of activating control type. Thus the controllers and cofactors are added to input
+		places of the transition.
+
+		If the direction is reversible, the transition gets duplicated.
+		"""
+		# Create place for new controller or cofactor
+		place = self.net.create_place(split_uri(control.participant)[1], control.participantName)
+		# Get all transitions which are instances of controlled
+		transitions = self.get_transitions(control)
+
+		for transition in transitions:
+			self.connect(transition, place)
+
+		#TODO finish this stuff
+		raise NotImplementedError
+
+
+class InhibitionIrreversible(BiopaxConverter):
+	def __init__(self, graph, petri_net):
+		"""Converts members of class Control with controlType Inhibition, Inhibition-Alossteric and Activation-Alossteric from BioPAX to a Petri Net.
+
+		:param graph: rdflib.Graph
+		:param petri_net: PetriNet
+		"""
+		BiopaxConverter.__init__(self, graph, petri_net)
+
+	query = """
+		PREFIX bp: <http://www.biopax.org/release/biopax-level3.owl#>
+		SELECT *
+		WHERE {
+			?controlClass rdfs:subClassOf+ bp:Control.
+
+			{ ?relation a bp:controller } UNION { ?relation a bp:cofactor }
+
+			?interaction
+				a ?controlClass;
+				bp:controlled ?controlled;
+				?relation ?participant.
+
+			OPTIONAL { ?participant bp:displayName ?participantName }
+			OPTIONAL { ?interaction bp:catalysisDirection ?direction }
+			{ ?interaction a bp:Catalysis }	UNION { ?interaction bp:controlType ?types } 
+			
+			VALUES (?types) { 
+			("INHIBITION-IRREVERSIBLE")
+			}
+		}
+	"""
+
+	def convert(self):
+		for control in self.graph.query(self.query):
+			self.add_control(control)
+
+	def add_control(self, control):
+		"""Adds a controller to a controlled transition.
+
+		Catalysis can only be of inhibition irreversible control type. Thus the controllers and cofactors are added to input
+		places of the transition.
+
+		If the direction is reversible, the transition gets duplicated.
+		"""
+		# Create place for new controller or cofactor
+		place = self.net.create_place(split_uri(control.participant)[1], control.participantName)
+		place2 = self.net.create_place(split_uri(control.participant)[1]+"_IRREVERSIBLE", control.participantName+"*")
+		# Create transitions necessairy
+		transitions = self.create_transitions(control)
+
+		#Create arcs
+		self.connect(transitions, place, place2)
+
+	def create_transitions(self, control):
+		"""
+		Creats 3 transitions which are neccesairy for the contruction of a irreversible controller
+		2 are created for the controlled
+		1 is created for the 2 instances of the controller
+
+		:rtype: Set(Transition)
+
+		"""
+		direction = self.get_direction(control)
+		transitions = []
+		transitions.append(self.net.create_transition(split_uri(control.controlled)[1],direction,split_uri(control.interaction)[1]))
+		transitions.append(self.net.create_transition(split_uri(control.controlled)[1],reverse(direction),split_uri(control.interaction)[1]))
+		transitions.append(self.net.create_transition(split_uri(control.participant)[1],Direction.left_to_right, split_uri(control.participant)[1]+"_IRREVERSIBLE",))
+
+		return transitions
+
+	def add_reversed_arcs(self, interaction, transition):
+		current_direction = []
+		ingoing_places = []
+		outgoing_places = []
+
+	def connect(self, transitions, place1, place2):
+		self.connect_both_ways(transitions[0],place1)
+		self.connect_both_ways(transitions[1],place2)
+
+		self.net.create_arc(place1,transitions[2])
+		self.net.create_arc(transitions[2],place2)
+
+	def connect_both_ways(self, transition, place):
+		self.net.create_arc(transition, place)
+		self.net.create_arc(place, transition)
+
+class NonUnCompetitiveOther(BiopaxConverter):
