eb6fa78ff6eee82f5f68d1bb139f715c69cd749e
Franz Geiger <mail@fx-g.de>
2014-07-20 02:04:58 +0200

    Implement BioPAX to Petri net conversion for activating control classes

--- Code/converter/converter.py
+++ Code/converter/converter.py
@@ -6,6 +6,10 @@ BP = Namespace('http://www.biopax.org/release/biopax-level3.owl')
 RTL = Literal('RIGHT-TO-LEFT', datatype=XSD.string)
 LTR = Literal('LEFT-TO-RIGHT', datatype=XSD.string)
 REVERSIBLE = Literal('REVERSIBLE', datatype=XSD.string)
+PHYSIOL_LTR = Literal('PHYSIOL-LEFT-TO-RIGHT', datatype=XSD.string)
+PHYSIOL_RTL = Literal('PHYSIOL-RIGHT-TO-LEFT', datatype=XSD.string)
+IRR_LTR = Literal('IRREVERSIBLE-LEFT-TO-RIGHT', datatype=XSD.string)
+IRR_RTL = Literal('IRREVERSIBLE-RIGHT-TO-LEFT', datatype=XSD.string)
 
 
 class BiopaxConverter(object):
@@ -73,7 +77,7 @@ class ConversionConverter(BiopaxConverter):
 		place = self.net.create_place(split_uri(conv.participant)[1], conv.participantName + location)
 
 		for transition in transitions:
-			self.connect(transitions, place, conv.relation)
+			self.connect(transition, place, conv.relation)
 
 	def connect(self, transition, place, relation):
 		# Default direction for Direction.unknown is left to right.
@@ -118,6 +122,7 @@ class ConversionConverter(BiopaxConverter):
 class ControlConverter(BiopaxConverter):
 
 	order = 10
+
 	def __init__(self, graph, petri_net):
 		"""Converts members of class Control from BioPAX to a Petri Net.
 
@@ -168,3 +173,107 @@ class ControlConverter(BiopaxConverter):
 		TODO: Is that the correct implementation?
 		"""
 		return NotImplementedError
+
+
+class ActivatingControlConverter(BiopaxConverter):
+
+	order = 100
+
+	def __init__(self, graph, petri_net):
+		"""Converts members of class Control from BioPAX to a Petri Net.
+
+		:param graph: rdflib.Graph
+		:param petri_net: PetriNet
+		"""
+		BiopaxConverter.__init__(self, graph, petri_net)
+
+	query = """
+		PREFIX bp: <http://www.biopax.org/release/biopax-level3.owl#>
+		SELECT *
+		WHERE {
+			?controlClass rdfs:subClassOf+ bp:Control.
+
+			{ ?relation a bp:controller } UNION { ?relation a bp:cofactor }
+
+			?interaction
+				a bp:controlClass;
+				bp:controlled ?controlled;
+				?relation ?participant.
+
+			OPTIONAL { ?participant bp:displayName ?participantName }
+			OPTIONAL { ?interaction bp:catalysisDirection ?direction }
+			OPTIONAL { ?interaction bp:controlType ?controlType }
+		}
+	"""
+
+	def convert(self):
+		for control in self.graph.query(self.query):
+			self.add_control(control)
+
+	def add_control(self, control):
+		"""Adds a controller to a controlled transition.
+
+		Catalysis can only by of activating control type. Thus the controllers and cofactors are added to input
+		places of the transition.
+
+		If the direction is reversible, the transition gets duplicated.
+		"""
+		place = self.net.create_place(split_uri(control.participant)[1], control.participantName)
+		transitions = self.get_transitions(control)
+
+		for transition in transitions:
+			self.connect(transition, place)
+
+	def connect(self, transition, place):
+		# Default direction for Direction.unknown is left to right.
+		if transition.direction is Direction.right_to_left:
+			self.net.create_arc(transition, place)
+		else:
+			self.net.create_arc(place, transition)
+
+	def get_transitions(self, control):
+		direction = self.get_direction(control)
+		uid = split_uri(control.interaction)[1]
+
+		transitions = []
+
+		if direction is Direction.left_to_right or direction is Direction.reversible:
+			transitions.append(self.get_transition_with_correct_direction(uid, Direction.left_to_right))
+
+		if direction is Direction.reversible:
+			transitions.append(self.net.create_transition(uid, Direction.right_to_left))
+
+		if direction is Direction.left_to_right:
+			transitions.append(self.get_transition_with_correct_direction(uid, Direction.right_to_left))
+
+		return transitions
+
+	def get_direction(self, control):
+		direction = Direction.unknown
+
+		if control.direction is REVERSIBLE:
+			direction = Direction.reversible
+
+		if control.direction is PHYSIOL_LTR or control.direction is IRR_LTR:
+			direction = Direction.left_to_right
+
+		if control.direction is PHYSIOL_RTL or control.direction is IRR_RTL:
+			direction = Direction.right_to_left
+
+		return direction
+
+	def get_transition_with_correct_direction(self, uid, direction):
+
+		if self.net.transitions.get((uid, Direction.unknown)):
+			return self.update_transition_direction(uid, Direction.unknown, direction)
+		else:
+			return self.net.create_transition(uid, Direction.left_to_right)
+
+	def update_transition_direction(self, uid, old_direction, new_direction):
+		transition = self.transitions[(uid, old_direction)]
+		transition.direction = new_direction
+
+		self.transitions.pop((uid, old_direction))
+		self.transitions[(uid, new_direction)] = transition
+
+		return transition
